6cb0020068339c2e34e13985703e9995df66cc88
Tony Enerson <tony@enerson.ca>
2021-05-19 11:59:42 -0600

    Start DetailComponent

--- client/src/pages/ListPage.js
+++ client/src/pages/ListPage.js
@@ -1,9 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import './ListPage.css';
 
+const SuperheroDetail = ({ superheroId }) => {
+  const [superhero, setSuperhero] = useState({
+    name: "",
+    nickname: "",
+    alterego: "",
+    sidekick: ""
+  })
+
+  useEffect(() => {
+    const getSuperhero = async () => {
+      // fetch uses the "proxy" value set in client/package.json
+      let response = await fetch('/superhero/'+superheroId);
+      let data = await response.json();
+      setSuperhero(data);
+    };
+    getSuperhero();
+}, [superheroId])
+
+  return (
+    <div>
+      <div>Name:  {superhero.name} </div>
+      <div>Nickname:  {superhero.nickname} </div>
+      <div>Alter Ego:  {superhero.alterego} </div>
+      <div>Sidekick:  {superhero.sidekick} </div>
+    </div>
+  )
+}
+
 const ListPage = () => {
     const [rows, setRows] = useState([]);
-  
+    const [selectedSuperheroId, setSelectedSuperheroId] = useState()
+
     useEffect(() => {
       const getSuperheroes = async () => {
         // fetch uses the "proxy" value set in client/package.json
@@ -21,7 +50,7 @@ const ListPage = () => {
               <tr><th>Name</th><th>Nickname</th><th>Alter Ego</th><th>Sidekick</th></tr>
               {rows.map((row) => {
                   return (
-                    <tr key={row.name}>
+                    <tr key={row.name} onClick={() => setSelectedSuperheroId(row._id)}>
                         <td>{row.name}</td>
                         <td>{row.nickname}</td>
                         <td>{row.alterego}</td>
@@ -31,6 +60,8 @@ const ListPage = () => {
               })}                
             </tbody>
         </table>
+
+        { selectedSuperheroId && <SuperheroDetail superheroId={ selectedSuperheroId }/> }
       </div>
     )
   }
